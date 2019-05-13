b582255bc8b3fd284178a517c8f019ab980b94c6
Runamook <tothe8c@gmail.com>
2019-05-13 18:01:31 +0300

    App mod

--- Act_dlms/emh_to_zabbix.py
+++ Act_dlms/emh_to_zabbix.py
@@ -2,16 +2,20 @@ from Act_dlms.emhmeter import get_data_15
 from multiprocessing.dummy import Pool
 import requests
 from pyzabbix import ZabbixMetric, ZabbixSender
-
+from Act_dlms.Helpers.create_logger import create_logger
 
 # TODO: Add transforms
 
 
+logger = create_logger("/var/log/emh_to_zabbix.log", "MainApp", loglevel="INFO")
+
+
 def get_json():
     url = "http://10.11.30.97:5000/api/meterpinginfo"
+    logger.debug(f"Connecting to {url}")
     response = requests.get(url)
-    assert response.status_code == 200, self.logger.error("API responded %s, expected 200" % response.status_code)
-
+    assert response.status_code == 200, logger.error("API responded %s, expected 200" % response.status_code)
+    # assert response.status_code == 200, f"API responded {response.status_code}, expected 200"
     return response.json()
 
 
@@ -29,7 +33,7 @@ def create_metrics(data, meter_data):
 
     for data_set in data:
         for metric_time in data_set:
-            for metric_tuple in data_set[dt]:
+            for metric_tuple in data_set[metric_time]:
                 metric_key = metric_tuple[0]
                 metric_value = metric_tuple[1]
 
@@ -40,7 +44,7 @@ def create_metrics(data, meter_data):
 
 def push_data(meter_data):
     """
-    :param meter_data: {
+    meter_data: {
     'meterNumber': '04682656',
     'manufacturer': 'EMH',
     'ip': '10.124.0.62',
@@ -54,22 +58,38 @@ def push_data(meter_data):
 
     data = get_data_15(meter_data["ip"])
     metrics = create_metrics(data, meter_data)
-    sender = ZabbixSender(zabbix_server_ip)
+    # sender = ZabbixSender(zabbix_server_ip)
+    sender = ZabbixSender("127.0.0.1")
     zabbix_response = sender.send(metrics)
 
     if zabbix_response.failed > 0 and zabbix_response.processed == 0:
-        self.logger.error("Something went totally wrong, terminating\n%s" % zabbix_response)
+        # self.logger.error("Something went totally wrong, terminating\n%s" % zabbix_response)
+        print(f"Something went totally wrong, terminating\n{zabbix_response}")
         exit(1)
     elif zabbix_response.failed > 0 and zabbix_response.failed > zabbix_response.processed:
-        self.logger.warning("More failures that successes %s" % zabbix_response)
+        # self.logger.warning("More failures that successes %s" % zabbix_response)
+        print(f"More failures that successes {zabbix_response}")
     else:
-        self.logger.warning("Result %s" % zabbix_response)
+        # self.logger.warning("Result %s" % zabbix_response)
+        print(f"Result {zabbix_response}")
     return
 
 
 def meta_15():
-    pool = Pool(32)
-    list_of_meters = get_json()
+    logger.info(f"Starting app")
+    pool = Pool(16)
+    # list_of_meters = get_json()
+    list_of_meters = {
+        "meterNumber":"05296170",
+        "manufacturer":"EMH",
+        "ip":"10.124.2.120",
+        "installationDate":"2019-02-20T09:00:00",
+        "isActive":True,
+        "voltageRatio":200,
+        "currentRatio":15,
+        "totalFactor":215
+    }
+    logger.debug(f"Found {len(list_of_meters)} meters")
     pool.map(push_data, list_of_meters)
 
 
